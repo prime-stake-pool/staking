a3bd19eab19ef8e05e08e5a8c53a618eb209491e
prime-stake-pool <rajasanc@gmail.com>
2025-11-29 17:13:06 +0530

    Update cardano_serialization_lib.js

--- public/libs/cardano_serialization_lib.js
+++ public/libs/cardano_serialization_lib.js
@@ -1,4 +1,5 @@
 import * as wasm from "./cardano_serialization_lib_bg.wasm";
+export * from "./cardano_serialization_lib_bg.js";
 import { __wbg_set_wasm } from "./cardano_serialization_lib_bg.js";
 __wbg_set_wasm(wasm);
-export * from "./cardano_serialization_lib_bg.js";
+
